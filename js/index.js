b36187eb28c6782a56f36f2a016740c1905ed9fc
Rously Paul <vegeta224@gmail.com>
2021-11-20 18:34:29 -0700

    add img tag

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function cycle() {
         nextImage = imgList[index];
         const ourSrc = preloadImage(nextImage.src);
         //imgElement.src = ourSrc;
-        imgElement.appendChild(ourSrc)
+        imgElement.appendChild(ourSrc.img)
         (index > 1 ? index = 0 : index++)
     }, 3000)
 }
