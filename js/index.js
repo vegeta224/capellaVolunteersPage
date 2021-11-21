0b2cfcc980bdabb9ad0414aae98d34c1a01553f1
Rously Paul <vegeta224@gmail.com>
2021-11-20 20:23:50 -0700

    try

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ let index = 0;
 imgList = ['./images/banner1.jpg', './images/banner2.jpg', './images/banner3.jpg'];
 
 function preloadImage(url){
-    console.log('running preload')
     const img = new Image();
     img.src = url;
     document.getElementById('banners').src == img
@@ -13,22 +12,18 @@ function onpageload(images) {
     for (var i = 0; i < images.length; i++) {
       images[i] = preloadImage(images[i])
     }
-    console.log('onpageload')
     return images
 };
 
 function running() {
-    console.log('running')
     onpageload(imgList);
-   cycle();
+    cycle();
 }
 
 function cycle() {
- //   console.log('cycle')
     const imgElement = document.getElementById('banners')
     setInterval(function() {
-        nextImage = imgList[index];
-        //console.log(imgElement.src, nextImage);        
+        nextImage = imgList[index];        
         imgElement.src = nextImage.src;
         (index > 1 ? index = 0 : index++)
     }, 3000)
